a78dfcfb71e12bbdc10b032e52dc0be86acb09a8
Palukuri <ppalukuri@galaxe.com>
2025-08-06 12:41:01 +0530

    first commit

---
+++ cypress/e2e/Practice/login.cy.js
@@ -0,0 +1,23 @@
+describe('My First Test', () => {
+it('Login functionality',()=>{
+
+    cy.visit("https://practice.expandtesting.com/login")
+    cy.get('#username').click().type("practice")
+    cy.get('#password').type("SuperSecretPassword!")
+    cy.get('#login > .btn').click()
+  
+  })
+
+    it('Logout functionality',()=>{
+
+    cy.visit("https://practice.expandtesting.com/login")
+    cy.get('#username').click().type("practice")
+    cy.get('#password').type("SuperSecretPassword!")
+    cy.get('#login > .btn').click()
+
+    cy.get('.icon-2x').click()
+   
+  
+  })
+
+})
